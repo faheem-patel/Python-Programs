9249ad1800bd9189d257ac77fe64a1e3659d7a81
Faheem Patel <patelfaheem@outlook.com>
2024-11-29 12:50:37 +0530

    no message

---
+++ multiplication-table.py
@@ -0,0 +1,8 @@
+num =int(input('Enter the number'))
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+    print(num, 'x', i, '=', num*i)
